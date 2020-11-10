51d30999a1480ef07663d06ec4cb1dec58ff387b
sagar7 <sagarg1998@gmail.com>
2020-11-10 19:38:10 +0530

    Doctor UI finished

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,6 @@
-var Healthcare = artifacts.require("./Healthcare.sol");
 var Hospital = artifacts.require("./Hospital.sol");
 var Patient = artifacts.require("./Patient.sol");
 module.exports = function(deployer) {
-  deployer.deploy(Healthcare);
   deployer.deploy(Hospital);
   deployer.deploy(Patient);
 };
